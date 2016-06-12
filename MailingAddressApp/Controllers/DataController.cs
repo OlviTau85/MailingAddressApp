6c17e2d3eeaab48c935eca7896e9f0cbc2551a63
olvitau <olvitau@gmail.com>
2016-06-12 20:39:32 +0400

    creation database and data controller and view table

---
+++ MailingAddressApp/Controllers/DataController.cs
@@ -0,0 +1,26 @@
+﻿using MailingAddressApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MailingAddressApp.Controllers
+{
+    public class DataController : Controller
+    {
+        //
+        // GET: /Data/
+
+        public JsonResult GetMailAddressList()
+        {
+            List<MailAddress> adsList = new List<MailAddress>();
+            using (MailAddressEntities dc = new MailAddressEntities())
+            {
+                adsList = dc.MailAddresses.OrderBy(ord => ord.CreationDate).ToList();
+            }
+            return new JsonResult { Data = adsList, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+    }
+}
