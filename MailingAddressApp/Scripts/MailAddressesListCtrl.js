e4c31e9488f137750e4f932cfb5c786e71118e64
olvitau <olvitau@gmail.com>
2016-06-17 00:52:42 +0400

    add sorting by all fields

--- MailingAddressApp/Scripts/MailAddressesListCtrl.js
+++ MailingAddressApp/Scripts/MailAddressesListCtrl.js
@@ -1,9 +1,27 @@
-﻿app.controller('MailAddressesListCtrl', function ($scope, MailAddressesListModel) {
+﻿app.controller('MailAddressesListCtrl', ['$scope', 'MailAddressesListModel', function ($scope, MailAddressesListModel) {
     $scope.mailAddressesList = null;
     MailAddressesListModel.GetMailAddressesList().then(function (data) {
         $scope.mailAddressesList = data.data;
     }, function (error) {
         alert("Error! Can't get mail address list!");
     });
-})
+
+    $scope.sortField = undefined;
+    $scope.reverse = false;
+
+    $scope.sort = function (fieldName) {
+        if ($scope.sortField === fieldName) {
+            $scope.reverse = !$scope.reverse;
+        } else {
+            $scope.sortField = fieldName;
+            $scope.reverse = false;
+        }
+    };
+    $scope.isSortUp = function (fieldName) {
+        return $scope.sortField === fieldName && !$scope.reverse;
+    };
+    $scope.isSortDown = function (fieldName) {
+        return $scope.sortField === fieldName && $scope.reverse;
+    };
+}])
 .factory('MailAddressesListModel', MailAddressesListModel);
